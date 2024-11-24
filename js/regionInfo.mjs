3575ffa10ea29eb06becbfadf6d3a27ba29c62b1
Wizic7 <IsaacWizic7@gmail.com>
2024-11-23 19:23:31 -0700

    Added Url Links and started animal JSON. Will need to add more animals and maybe a dropdown menu later.

---
+++ js/regionInfo.mjs
@@ -0,0 +1,100 @@
+export const RegionData = 
+[
+    {
+        Name: "Antarctic",
+        Animals: [
+            {
+                Name: "Hunting Here is Illegal",
+                Image: "images/red_X.jpg",
+                Alt: "Illegal Sign",
+                PhotoCredit: {
+                    Name: "愚木混株 cdd20",
+                    Profile: "https://unsplash.com/@cdd20?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+                    Website: "Unsplash",
+                    Weblink: "https://unsplash.com/photos/a-cross-made-out-of-small-pieces-of-red-plastic-vR6bNYTVlpo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+                }
+      
+            }
+        ]
+    },
+    {
+        Name: "Americas",
+        Animals: [
+            {
+                Name: "White-tailed Deer (Odocoileus virginianus)",
+                Image: "images/Americas/white-tail.jpg",
+                Alt: "A white tail deer",
+                PhotoCredit: {
+                    Name: "Laura College",
+                    Profile: "https://unsplash.com/@laura_college?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+                    Website: "Unsplash",
+                    Weblink: "https://unsplash.com/photos/selective-focus-photography-of-brown-deer-standing-on-green-grass-field-during-daytime-K_Na5gCmh38?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+                }
+            }
+        ]
+    },
+    {
+        Name: "Europe",
+        Animals: [
+            {
+                Name: "",
+                Image: "",
+                Alt: "",
+                PhotoCredit: {
+                    Name: "",
+                    Profile: "",
+                    Website: "",
+                    Weblink: ""
+                }
+            }
+        ]
+    },
+    {
+        Name: "Africa",
+        Animals: [
+            {
+                Name: "",
+                Image: "",
+                Alt: "",
+                PhotoCredit: {
+                    Name: "",
+                    Profile: "",
+                    Website: "",
+                    Weblink: ""
+                }
+            }
+        ]
+    },
+    {
+        Name: "Asia",
+        Animals: [
+            {
+                Name: "",
+                Image: "",
+                Alt: "",
+                PhotoCredit: {
+                    Name: "",
+                    Profile: "",
+                    Website: "",
+                    Weblink: ""
+                }
+            }
+        ]
+    },
+    {
+        Name: "Oceania",
+        Animals: [
+            {
+                Name: "",
+                Image: "",
+                Alt: "",
+                PhotoCredit: {
+                    Name: "",
+                    Profile: "",
+                    Website: "",
+                    Weblink: ""
+                }
+            }
+        ]
+    },
+];
